2ee330d434bd1c5a4197d5fe2717631a8af14910
Luffy5522 <Luffy5522@163.com>
2023-02-24 20:16:38 +0800

    day01-parent和model模块的创立

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/Impl/CourseBaseInfoServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/Impl/CourseBaseInfoServiceImpl.java
@@ -1,14 +1,18 @@
 package com.xuecheng.content.service.Impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.mapper.CourseBaseMapper;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 import com.xuecheng.content.service.CourseBaseInfoService;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @Author Luffy5522
@@ -29,9 +33,30 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
         LambdaQueryWrapper<CourseBase> queryWrapper = new LambdaQueryWrapper<>();
 
         // 对审核状态进行查询
+        queryWrapper.eq(Strings.isNotEmpty(queryCourseParamsDto.getAuditStatus()),
+                CourseBase::getAuditStatus, queryCourseParamsDto.getAuditStatus());
 
+        // 对课程名称进行模糊查询
+        queryWrapper.like(Strings.isNotEmpty(queryCourseParamsDto.getCourseName()),
+                CourseBase::getName, queryCourseParamsDto.getCourseName());
 
+        // 对发布状态进行查询
+        queryWrapper.like(Strings.isNotEmpty(queryCourseParamsDto.getPublishStatus()),
+                CourseBase::getStatus, queryCourseParamsDto.getPublishStatus());
 
-        return null;
+        //分页对象
+        Page<CourseBase> page = new Page<>(pageParams.getPageNo(),
+                pageParams.getPageSize());
+
+        // 查询数据内容获得结果
+        Page<CourseBase> courseBasePage = courseBaseMapper.selectPage(page, queryWrapper);
+
+        // 获得查询结果的列表
+        List<CourseBase> list = courseBasePage.getRecords();
+
+        long total = courseBasePage.getTotal();
+
+        return new PageResult<>(list, total,
+                pageParams.getPageNo(),pageParams.getPageSize());
     }
 }
--- xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/XuechengPlusContentServiceApplicationTests.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/XuechengPlusContentServiceApplicationTests.java
@@ -1,7 +1,11 @@
 package com.xuecheng.content;
 
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +17,29 @@ class XuechengPlusContentServiceApplicationTests {
     @Autowired
     CourseBaseMapper courseBaseMapper;
 
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
+
     @Test
     void contextLoads() {
         CourseBase courseBase = courseBaseMapper.selectById(74L);
         Assertions.assertNotNull(courseBase);
     }
 
+    @Test
+    void testQueryCourseBaseList() {
+        PageParams pageParams = new PageParams(1, 10);
+        QueryCourseParamsDto queryCourseParamsDto = new QueryCourseParamsDto();
+        queryCourseParamsDto.setCourseName("java");
+        queryCourseParamsDto.setAuditStatus("");
+        queryCourseParamsDto.setPublishStatus("");
+        PageResult<CourseBase> courseBasePageResult = courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParamsDto);
+        System.out.println(courseBasePageResult.toString());
+
+
+    }
+
+
 }
+
+
